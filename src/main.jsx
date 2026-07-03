155b051e61869e0fc8d9dd1911bf70aba999aed9
Jayamurali2608 <jayamurali2608@gmail.com>
2026-07-03 11:48:31 +0530

    changed to hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-import React from "react";
+import {HashRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-  <BrowserRouter>
+  <HashRouter>
     <App />
-   </BrowserRouter>
-  </React.StrictMode>
+   </HashRouter>
   
 );
